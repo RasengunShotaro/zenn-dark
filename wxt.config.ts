8d02ac8214c4934879eba4766dddeeabf0437487
RasengunShotaro <scarlet.mori0318@gmail.com>
2024-09-10 10:50:34 +0900

    👍 idを追加

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 export default defineConfig({
-  manifest:{
+  manifest: {
     manifest_version: 3,
     name: "Zenn Dark Reader",
     version: "0.1.0",
-    description: "zenn.devをダークモードに対応させる拡張機能です。"
-  }
+    description: "zenn.devをダークモードに対応させる拡張機能です。",
+    browser_specific_settings: {
+      gecko: {
+        id: "zenndark@rasengun.dev",
+      },
+    },
+  },
 });
